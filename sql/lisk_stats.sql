6fa854aaff0ae0d9bf9e4b4f74f639b3bbff6f36
Quyse Lert <quyse0@gmail.com>
2018-05-22 12:50:40 +0300

    payments count for lisk; also fix units for median value and fees

--- sql/lisk_stats.sql
+++ sql/lisk_stats.sql
@@ -2,20 +2,16 @@
 -- genesis block time is 2016-05-24 20:00:00
 CREATE MATERIALIZED VIEW lisk_stats AS (
   WITH
-    blocks_stats AS (SELECT
-      DATE_TRUNC('day', TIMESTAMP '2016-05-24 20:00:00' + block."timestamp" * INTERVAL '1 second') "date",
-      SUM(block."numberOfTransactions") "cnt",
-      SUM(block."totalAmount") :: NUMERIC * 0.00000001 "value",
-      SUM(block."totalFee") :: NUMERIC * 0.00000001 "fees"
-      FROM blocks block
-      GROUP BY "date"
-      ),
     tx_stats AS (SELECT
       DATE_TRUNC('day', TIMESTAMP '2016-05-24 20:00:00' + tx."timestamp" * INTERVAL '1 second') "date",
-      PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY tx."amount") "med_value",
-      PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY tx."fee") "med_fees",
+      COUNT(*) "cnt",
+      SUM(tx."amount") :: NUMERIC * 0.00000001 "value",
+      SUM(tx."fee") :: NUMERIC * 0.00000001 "fees",
+      PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY tx."amount" :: NUMERIC * 0.00000001) "med_value",
+      PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY tx."fee" :: NUMERIC * 0.00000001) "med_fees",
       COUNT(DISTINCT tx."senderId") "from_cnt",
-      COUNT(DISTINCT tx."recipientId") "to_cnt"
+      COUNT(DISTINCT tx."recipientId") "to_cnt",
+      SUM(CASE WHEN tx."type" = 0 THEN 1 ELSE 0 END) "payment_cnt"
       FROM trs tx
       GROUP BY "date"
       ),
@@ -37,17 +33,17 @@ CREATE MATERIALIZED VIEW lisk_stats AS (
       GROUP BY "date"
       )
     SELECT
-      blocks_stats."date" "date",
-      blocks_stats."cnt" "cnt",
-      blocks_stats."value" "value",
-      blocks_stats."fees" "fees",
+      tx_stats."date" "date",
+      tx_stats."cnt" "cnt",
+      tx_stats."value" "value",
+      tx_stats."fees" "fees",
       tx_stats."med_value" "med_value",
       tx_stats."med_fees" "med_fees",
       tx_stats."from_cnt" "from_cnt",
       tx_stats."to_cnt" "to_cnt",
+      tx_stats."payment_cnt" "payment_cnt",
       addr_stats."addr_cnt" "addr_cnt"
-    FROM blocks_stats
-    LEFT JOIN tx_stats ON blocks_stats."date" = tx_stats."date"
-    LEFT JOIN addr_stats ON blocks_stats."date" = addr_stats."date"
-    ORDER BY blocks_stats."date"
+    FROM tx_stats
+    LEFT JOIN addr_stats ON tx_stats."date" = addr_stats."date"
+    ORDER BY tx_stats."date"
   ) WITH NO DATA;
